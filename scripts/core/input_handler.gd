f6b32121025c0bc1ac98b87d5f1ff8fd1fced177
axxxiomatic <pinkod.miet16@gmail.com>
2025-05-31 01:30:53 +0300

    Update input_handler.gd

--- scripts/core/input_handler.gd
+++ scripts/core/input_handler.gd
@@ -1,17 +1,13 @@
 class_name InputHandler
 
-# Паттерн "Команда"
 var commands := {}
 
-# Постановка входящей команды в очередь
 func register_command(key: String, command: Callable):
 	commands[key] = command
 
-# Исполнение команды
 func execute_command(key: String):
 	if commands.has(key):
 		commands[key].call()
 
-# Очистка очереди команд
 func clear_commands():
 	commands.clear()
