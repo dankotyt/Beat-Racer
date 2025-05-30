81b5ac3b1302df2d1fe43d758a9972fe21a9893b
axxxiomatic <pinkod.miet16@gmail.com>
2025-05-31 01:24:08 +0300

    Update tree_obstacle_left.gd

--- elements/obstacles/tree_obstacle/tree_obstacle_left.gd
+++ elements/obstacles/tree_obstacle/tree_obstacle_left.gd
@@ -1,12 +1,9 @@
-# tree_obstacle.gd (используется для left и right)
 extends Area2D
 
 var speed := 500.0
 
 func _ready():
-	# Принудительно установить нужную позицию по X
-		position.x = 170
-
+	position.x = 170
 
 func _process(delta):
 	if GameManager.state != GameManager.GameState.PLAYING:
@@ -21,5 +18,4 @@ func _on_body_entered(body):
 	if body.name == "Player":
 		GameManager.state = GameManager.GameState.GAME_OVER
 		GameManager.emit_signal("game_over")
-		
 		
