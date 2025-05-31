940559116d877920ae157eeed09160ce4ea05ea6
dankotyt <danilkotlarov1@gmail.com>
2025-05-31 14:28:46 +0300

    Пофикшена ошибка, когда после перезапуска дорога не двигалась, а препятствия не генерировались

--- elements/screens/win_screen.gd
+++ elements/screens/win_screen.gd
@@ -1,4 +1,7 @@
 extends Node
 
+func _ready() -> void:
+	GameManager.state = GameManager.GameState.PLAYING
+
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/scn_game_menu.tscn")
